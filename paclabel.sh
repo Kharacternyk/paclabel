7fe76cc075d1695bfbf135d4e2ebf3169876fb24
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-04-02 13:09:27 +0300

    Proof of concept.

--- paclabel.sh
+++ paclabel.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
-PACMAN_INVOCATION='pacman'
+[[ $1 =~ -(.).* ]] && MODE=${BASH_REMATCH[1]}
+echo $MODE
 
-while [[ $# != 0 ]]; do
-    if [[ $1 =~ ^(.*):(.*)$ ]]; then
-        PACKAGE=${BASH_REMATCH[1]}
-        LABEL=${BASH_REMATCH[2]}
-        echo 'Package:' $PACKAGE
-        echo 'Label:' $LABEL
-        PACMAN_INVOCATION+=" $PACKAGE"
-        shift
-    else
-        PACMAN_INVOCATION+=" $1"
-        shift
-    fi
-done
+case $MODE in
+    S)
+        PACMAN_INVOCATION='pacman'
+        while [[ $# != 0 ]]; do
+            if [[ $1 =~ ^(.+):(.+)$ ]]; then
+                PACKAGE=${BASH_REMATCH[1]}
+                LABEL=${BASH_REMATCH[2]}
+                PACMAN_INVOCATION+=" $PACKAGE"
+                echo $PACKAGE:$LABEL >> /tmp/labels
+                shift
+            else
+                PACMAN_INVOCATION+=" $1"
+                shift
+            fi
+        done
+        echo $PACMAN_INVOCATION
+        ;;
+    Q)
+        [[ $1 == *q* ]] && pacman $@ && exit
 
-echo $PACMAN_INVOCATION
+        IS_VERSION=0
+        for PKG in $(pacman $@); do
+            if [[ $IS_VERSION == 1 ]]; then
+                IS_VERSION=0
+                printf ' %s\n' $PKG
+            else
+                IS_VERSION=1
+                printf '%s' $PKG
+                LABEL="$(grep $PKG /tmp/labels)"
+                [[ -n $LABEL ]] && printf " : %s" $LABEL
+            fi
+        done
+        ;;
+    *) ;;
+
+esac
